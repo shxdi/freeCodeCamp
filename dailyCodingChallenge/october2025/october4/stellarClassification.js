2e218bcbc964a70e8d5501caf643c442d0f81cf2
shxdi <234461025+shxdi@users.noreply.github.com>
2025-10-04 15:11:01 -0500

    Add instructions for challenge

--- dailyCodingChallenge/october2025/october4/stellarClassification.js
+++ dailyCodingChallenge/october2025/october4/stellarClassification.js
@@ -1,3 +1,15 @@
+/*For today's challenge, you are given the surface temperature of a star in Kelvin (K) and need to determine its stellar classification based on the following ranges:
+
+"O": 30,000 K or higher
+"B": 10,000 K - 29,999 K
+"A": 7,500 K - 9,999 K
+"F": 6,000 K - 7,499 K
+"G": 5,200 K - 5,999 K
+"K": 3,700 K - 5,199 K
+"M": 0 K - 3,699 K
+
+Return the classification of the given star.*/
+
 function classification(temp) {
   return temp;
 }
