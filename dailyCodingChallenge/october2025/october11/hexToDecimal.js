0fa5cdb89aa415d377a48c05b9837bbcff17f3d6
shxdi <234461025+shxdi@users.noreply.github.com>
2025-10-11 09:05:42 -0500

    Add instructions for challenge

--- dailyCodingChallenge/october2025/october11/hexToDecimal.js
+++ dailyCodingChallenge/october2025/october11/hexToDecimal.js
@@ -1,3 +1,27 @@
+/*Given a string representing a hexadecimal number (base 16), return its decimal (base 10) 
+value as an integer.
+
+Hexadecimal is a number system that uses 16 digits:
+0-9 represent values 0 through 9.
+A-F represent values 10 through 15.
+
+Here's a partial conversion table:
+Hexadecimal	Decimal
+0	0
+1	1
+...	...
+9	9
+A	10
+...	...
+F	15
+10	16
+...	...
+9F	159
+A0	160
+...	...
+FF	255
+100	256*/
+
 function hexToDecimal(hex) {
   return hex;
 }
