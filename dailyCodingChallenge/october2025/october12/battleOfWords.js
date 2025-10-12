fcd5def8146ee80c162ee5a0ad92e5fda7612766
shxdi <234461025+shxdi@users.noreply.github.com>
2025-10-12 09:02:47 -0500

    Add provided information

--- dailyCodingChallenge/october2025/october12/battleOfWords.js
+++ dailyCodingChallenge/october2025/october12/battleOfWords.js
@@ -1,1 +1,30 @@
- 
+/*Given two sentences representing your team and an opposing team, where each word from 
+your team battles the corresponding word from the opposing team, determine which team wins 
+using the following rules:
+
+-The given sentences will always contain the same number of words.
+-Words are separated by a single space and will only contain letters.
+-The value of each word is the sum of its letters.
+-Letters a to z correspond to the values 1 through 26. For example, a is 1, and z is 26.
+-A capital letter doubles the value of the letter. For example, A is 2, and Z is 52.
+-Words battle in order: the first word of your team battles the first word of the opposing team, 
+and so on.
+-A word wins if its value is greater than the opposing word's value.
+-The team with more winning words is the winner.
+
+Return "We win" if your team is the winner, "We lose" if your team loses, and "Draw" if both 
+teams have the same number of wins. */
+
+function battle(ourTeam, opponent) {
+  return ourTeam;
+}
+
+/*
+1. battle("hello world", "hello word") should return "We win".
+2. battle("Hello world", "hello world") should return "We win".
+3. battle("lorem ipsum", "kitty ipsum") should return "We lose".
+4. battle("hello world", "world hello") should return "Draw".
+5. battle("git checkout", "git switch") should return "We win".
+6. battle("Cheeseburger with fries", "Cheeseburger with Fries") should return "We lose".
+7. battle("We must never surrender", "Our team must win") should return "Draw". 
+*/
