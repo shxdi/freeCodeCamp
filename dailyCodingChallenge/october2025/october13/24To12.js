ea5f2a94b88f2e267dcf2f87d67b61d9ed24e6d2
shxdi <234461025+shxdi@users.noreply.github.com>
2025-10-13 11:51:31 -0500

    Finish 24To12

--- dailyCodingChallenge/october2025/october13/24To12.js
+++ dailyCodingChallenge/october2025/october13/24To12.js
@@ -14,6 +14,69 @@ from "0000" to "2359".
 5. to12("0030") should return "12:30 AM".
 */
 
+// Variable for finished string
+let timeIn12;
+
 function to12(time) {
-  return time;
+  console.log(time.slice(0, 2));
+  // For AM or PM
+  let additional;
+
+  if (time.slice(0, 2) === '00') {
+    console.log('working');
+    additional = ' AM';
+
+    // Remove 12 hours if after 1pm
+    time = '12';
+
+    // Get final string
+    combineString(time, additional);
+  } else if (time < 1200) {
+    additional = ' AM';
+
+    // Get final string
+    combineString(time, additional);
+  } else if (time > 1159 && time < 1300) {
+    additional = ' PM';
+
+    // Get final string
+    combineString(time, additional);
+  } else if (time > 1259) {
+    additional = ' PM';
+
+    // Remove 12 hours if after 1pm
+    time -= 1200;
+
+    // Get final string
+    combineString(time, additional);
+  }
+
+  return timeIn12;
+}
+
+function combineString(time, additional) {
+  // Turn time into a string
+  time = time.toString();
+
+  // Seperate hours and minutes
+  let hour;
+  let minute;
+  // If time has 4 digits
+  if (time.slice(3) !== '') {
+    hour = time.slice(0, 2);
+    minute = time.slice(2, 4);
+
+    // If time has 3 digits
+  } else if (time.slice(3) === '' || time.slice(0) === '0') {
+    hour = time.slice(0, 1);
+    minute = time.slice(1, 3);
+  }
+
+  // If first number in string is 0
+  if (hour.slice(0, 1) === '0') {
+    hour = hour.slice(1);
+  }
+
+  // Combine finished string
+  timeIn12 = hour + ':' + minute + additional;
 }
