80451214db6ceef7df72602c013dd85c372241cc
shxdi <234461025+shxdi@users.noreply.github.com>
2025-10-14 09:52:21 -0500

    Add given information

--- dailyCodingChallenge/october2025/october14/stringCount.js
+++ dailyCodingChallenge/october2025/october14/stringCount.js
@@ -1,1 +1,18 @@
- 
+/*
+ Given two strings, determine how many times the second string appears in the first.
+
+The pattern string can overlap in the first string. For example, "aaa" contains "aa" twice. 
+The first two a's and the second two.
+*/
+
+/*
+1. count('abcdefg', 'def') should return 1.
+2. count('hello', 'world') should return 0.
+3. count('mississippi', 'iss') should return 2.
+4. count('she sells seashells by the seashore', 'sh') should return 3.
+5. count('101010101010101010101', '101') should return 10.
+*/
+
+function count(text, pattern) {
+  return text;
+}
