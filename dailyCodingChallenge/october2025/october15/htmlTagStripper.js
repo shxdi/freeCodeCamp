05edf50644f36869b943de8987e8dde4f51d6610
shxdi <234461025+shxdi@users.noreply.github.com>
2025-10-15 08:47:09 -0500

    Add provided information

--- dailyCodingChallenge/october2025/october15/htmlTagStripper.js
+++ dailyCodingChallenge/october2025/october15/htmlTagStripper.js
@@ -1,1 +1,20 @@
- 
+/*
+Given a string of HTML code, remove the tags and return the plain text content.
+
+The input string will contain only valid HTML.
+HTML tags may be nested.
+Remove the tags and any attributes.
+For example, '<a href="#">Click here</a>' should return "Click here". 
+*/
+
+/*
+1. stripTags('<a href="#">Click here</a>') should return "Click here".
+2. stripTags('<p class="center">Hello <b>World</b>!</p>') should return "Hello World!".
+3. stripTags('<img src="cat.jpg" alt="Cat">') should return an empty string ("").
+4. stripTags('<main id="main"><section class="section">section</section><section class=
+    "section">section</section></main>') should return sectionsection.
+*/
+
+function stripTags(html) {
+  return html;
+}
