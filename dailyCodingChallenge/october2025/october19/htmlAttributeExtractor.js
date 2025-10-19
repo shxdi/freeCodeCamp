aba02cf541c75ec777576dff20e808d9e1eb3a52
shxdi <234461025+shxdi@users.noreply.github.com>
2025-10-19 09:05:16 -0500

    Add provided information

--- dailyCodingChallenge/october2025/october19/htmlAttributeExtractor.js
+++ dailyCodingChallenge/october2025/october19/htmlAttributeExtractor.js
@@ -1,1 +1,25 @@
- 
+/*
+Given a string of a valid HTML element, return the attributes of the element using the 
+following criteria:
+
+You will only be given one element.
+Attributes will be in the format: attribute="value".
+Return an array of strings with each attribute property and value, separated by a comma, 
+in this format: ["attribute1, value1", "attribute2, value2"].
+Return attributes in the order they are given.
+If no attributes are found, return an empty array.
+*/
+
+/*
+1. extractAttributes('<span class="red"></span>') should return ["class, red"].
+2. extractAttributes('<meta charset="UTF-8" />') should return ["charset, UTF-8"].
+3. extractAttributes("<p>Lorem ipsum dolor sit amet</p>") should return [].
+4. extractAttributes('<input name="email" type="email" required="true" />') should return 
+["name, email", "type, email", "required, true"].
+5. extractAttributes('<button id="submit" class="btn btn-primary">Submit</button>') should 
+return ["id, submit", "class, btn btn-primary"].
+*/
+
+function extractAttributes(element) {
+  return element;
+}
