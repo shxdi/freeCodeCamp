995ffc8dcf216cd9bc9a2b5d9666d21b400c325b
shxdi <234461025+shxdi@users.noreply.github.com>
2025-10-25 12:36:32 -0500

    Finish buildASentenceMaker

--- javaScript/buildASentenceMaker/sentenceMaker.js
+++ javaScript/buildASentenceMaker/sentenceMaker.js
@@ -1,1 +1,51 @@
- 
+let adjective = 'Big';
+let noun = 'Guy';
+let verb = 'Cold';
+let place = 'House';
+let adjective2 = 'Peppery';
+let noun2 = 'Hot Peppers';
+
+let firstStory =
+  'Once upon a time, there was a(n) ' +
+  adjective +
+  ' ' +
+  noun +
+  ' who loved to eat ' +
+  noun2 +
+  '. The ' +
+  noun +
+  ' lived in a ' +
+  place +
+  ' and had ' +
+  adjective2 +
+  ' nostrils that blew fire when it was ' +
+  verb +
+  '.';
+
+console.log('First story: ' + firstStory);
+
+adjective = 'Little';
+noun = 'Girl';
+verb = 'Cold';
+place = 'House';
+adjective2 = 'Not fire';
+noun2 = 'Sweet Peppers';
+
+let secondStory =
+  'Once upon a time, there was a(n) ' +
+  adjective +
+  ' ' +
+  noun +
+  ' who loved to eat ' +
+  noun2 +
+  '. The ' +
+  noun +
+  ' lived in a ' +
+  place +
+  ' and had ' +
+  adjective2 +
+  ' nostrils that blew fire when it was ' +
+  verb +
+  '.';
+
+console.log('Second story: ' + secondStory);
