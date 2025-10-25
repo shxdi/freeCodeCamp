c5d8380a4b45c5df36bb0ed38db1e82533f9e76f
shxdi <234461025+shxdi@users.noreply.github.com>
2025-10-25 11:33:27 -0500

    Finish buildAJavaScriptTriviaBot

--- javaScript/buildAJavaScriptTriviaBot/triviaBot.js
+++ javaScript/buildAJavaScriptTriviaBot/triviaBot.js
@@ -1,1 +1,26 @@
- 
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = 'Trivia Bot';
+let botLocation = 'A Place';
+let favoriteLanguage = 'JavaScript';
+
+console.log('My name is ' + botName + ' and I live on ' + botLocation + '.');
+console.log('My favorite programming language is ' + favoriteLanguage + '.');
+
+let codingFact =
+  favoriteLanguage + ' was created in just 10 Days (the first prototype)';
+console.log(codingFact);
+
+codingFact = favoriteLanguage + ' is not the same as Java';
+console.log(codingFact);
+
+codingFact = favoriteLanguage + ' is interpreted, not compiled';
+console.log(codingFact);
+
+console.log(
+  'It was fun sharing these facts with you. Goodbye! - ' +
+    botName +
+    ' from ' +
+    botLocation +
+    '.'
+);
