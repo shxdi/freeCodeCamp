ac83ee0cd6c7ecb6f01db4b410e19bd220f5bbb5
shxdi <234461025+shxdi@users.noreply.github.com>
2025-10-25 08:47:13 -0500

    Add provided information

--- dailyCodingChallenge/october2025/october25/complementaryDna.js
+++ dailyCodingChallenge/october2025/october25/complementaryDna.js
@@ -1,1 +1,20 @@
- 
+/*
+Given a string representing a DNA sequence, return its complementary strand using the 
+following rules:
+
+DNA consists of the letters "A", "C", "G", and "T".
+The letters "A" and "T" complement each other.
+The letters "C" and "G" complement each other.
+For example, given "ACGT", return "TGCA".
+*/
+
+/*
+1. complementaryDNA("ACGT") should return "TGCA".
+2. complementaryDNA("ATGCGTACGTTAGC") should return "TACGCATGCAATCG".
+3. complementaryDNA("GGCTTACGATCGAAG") should return "CCGAATGCTAGCTTC".
+4. complementaryDNA("GATCTAGCTAGGCTAGCTAG") should return "CTAGATCGATCCGATCGATC".
+*/
+
+function complementaryDNA(strand) {
+  return strand;
+}
