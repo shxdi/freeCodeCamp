0c090e97ac9e06332c6f4f146447ccec56030e71
shxdi <234461025+shxdi@users.noreply.github.com>
2025-10-26 08:19:51 -0500

    Add provided information

--- dailyCodingChallenge/october2025/october26/durationFormatter.js
+++ dailyCodingChallenge/october2025/october26/durationFormatter.js
@@ -1,1 +1,22 @@
- 
+/*
+Given an integer number of seconds, return a string representing the same duration in the 
+format "H:MM:SS", where "H" is the number of hours, "MM" is the number of minutes, and "SS" 
+is the number of seconds. Return the time using the following rules:
+
+- Seconds: Should always be two digits.
+- Minutes: Should omit leading zeros when they aren't needed. Use "0" if the duration is 
+less than one minute.
+- Hours: Should be included only if they're greater than zero.
+*/
+
+/*
+1. format(500) should return "8:20".
+2. format(4000) should return "1:06:40".
+3. format(1) should return "0:01".
+4. format(5555) should return "1:32:35".
+5. format(99999) should return "27:46:39".
+*/
+
+function format(seconds) {
+  return seconds;
+}
