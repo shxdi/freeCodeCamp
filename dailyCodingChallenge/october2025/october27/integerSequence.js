637594dd1bdd52e9e24c60cb4038106102676082
shxdi <234461025+shxdi@users.noreply.github.com>
2025-10-27 08:59:34 -0500

    Add provided information

--- dailyCodingChallenge/october2025/october27/integerSequence.js
+++ dailyCodingChallenge/october2025/october27/integerSequence.js
@@ -1,1 +1,17 @@
- 
+/*
+Given a positive integer, return a string with all of the integers from 1 up to, and including, 
+the given number, in numerical order.
+
+For example, given 5, return "12345".
+*/
+
+/*
+1. sequence(5) should return "12345".
+2. sequence(10) should return "12345678910".
+3. sequence(1) should return "1".
+4. sequence(27) should return "123456789101112131415161718192021222324252627".
+*/
+
+function sequence(n) {
+  return n;
+}
