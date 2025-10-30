58e75e847694ff187b6d1717cf657ebdc040a97f
shxdi <234461025+shxdi@users.noreply.github.com>
2025-10-30 09:15:24 -0500

    Add provided information

--- dailyCodingChallenge/october2025/october30/nthPrime.js
+++ dailyCodingChallenge/october2025/october30/nthPrime.js
@@ -1,1 +1,19 @@
- 
+/*
+A prime number is a positive integer greater than 1 that is divisible only by 1 and itself. 
+The first five prime numbers are 2, 3, 5, 7, and 11.
+
+Given a positive integer n, return the nth prime number. For example, given 5 return the 5th 
+prime number: 11.
+*/
+
+/*
+1. nthPrime(5) should return 11.
+2. nthPrime(10) should return 29.
+3. nthPrime(16) should return 53.
+4. nthPrime(99) should return 523.
+5. nthPrime(1000) should return 7919.
+*/
+
+function nthPrime(n) {
+  return n;
+}
