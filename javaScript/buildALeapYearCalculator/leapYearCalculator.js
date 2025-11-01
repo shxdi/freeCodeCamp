495a86e36c5921bf9a7de10104181da665511730
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-01 11:52:58 -0500

    Finish leapYearCalculator

--- javaScript/buildALeapYearCalculator/leapYearCalculator.js
+++ javaScript/buildALeapYearCalculator/leapYearCalculator.js
@@ -1,1 +1,17 @@
- 
+let year = 1000;
+let result;
+
+function isLeapYear(year) {
+  if (year % 400 === 0) {
+    return `${year} is a leap year.`;
+  } else if (year % 100 === 0) {
+    return `${year} is not a leap year.`;
+  } else if (year % 4 === 0) {
+    return `${year} is a leap year.`;
+  } else {
+    return `${year} is not a leap year.`;
+  }
+}
+
+result = isLeapYear(year);
+console.log(result);
