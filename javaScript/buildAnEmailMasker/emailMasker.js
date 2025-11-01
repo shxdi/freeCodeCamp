633442f3d87de30be8fd686ee1503ef723c67419
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-01 11:42:17 -0500

    Finish emailMasker

--- javaScript/buildAnEmailMasker/emailMasker.js
+++ javaScript/buildAnEmailMasker/emailMasker.js
@@ -1,1 +1,18 @@
- 
+let email = 'apple.pie@example.com';
+
+function maskEmail(email) {
+  let emailParts = email.split('@');
+  let username = emailParts[0];
+  let domain = emailParts[1];
+
+  let maskedUsername =
+    username[0] +
+    '*'.repeat(username.length - 2) +
+    username[username.length - 1];
+
+  let maskedEmail = maskedUsername + '@' + domain;
+
+  return maskedEmail;
+}
+
+console.log(maskEmail(email));
