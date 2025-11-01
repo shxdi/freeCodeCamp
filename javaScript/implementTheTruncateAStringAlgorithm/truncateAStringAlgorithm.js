4d87c54d958b1c20d8128c47e40fa2e1b6cdc25e
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-01 12:01:32 -0500

    Complete truncateAStringAlgorithm

--- javaScript/implementTheTruncateAStringAlgorithm/truncateAStringAlgorithm.js
+++ javaScript/implementTheTruncateAStringAlgorithm/truncateAStringAlgorithm.js
@@ -1,1 +1,14 @@
- 
+function truncateString(str, num) {
+  if (typeof str !== 'string') {
+    return 'Error: Please enter a valid string';
+  } else if (typeof num !== 'number') {
+    return 'Error: Please enter a valid number';
+  }
+
+  if (str.length > num) {
+    str = str.slice(0, num) + '...';
+    console.log(str);
+  }
+
+  return str;
+}
