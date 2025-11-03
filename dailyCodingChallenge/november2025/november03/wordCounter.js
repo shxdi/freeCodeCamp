f17384f9a2458fdca823a45a60a2ee5ceb2e004a
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-03 07:31:31 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november03/wordCounter.js
+++ dailyCodingChallenge/november2025/november03/wordCounter.js
@@ -1,1 +1,17 @@
- 
+/*
+Given a sentence string, return the number of words that are in the sentence.
+
+Words are any sequence of non-space characters and are separated by a single space.
+*/
+
+/*
+1. countWords("Hello world") should return 2.
+2. countWords("The quick brown fox jumps over the lazy dog.") should return 9.
+3. countWords("I like coding challenges!") should return 4.
+4. countWords("Complete the challenge in JavaScript and Python.") should return 7.
+5. countWords("The missing semi-colon crashed the entire internet.") should return 7.
+*/
+
+function countWords(sentence) {
+  return sentence;
+}
