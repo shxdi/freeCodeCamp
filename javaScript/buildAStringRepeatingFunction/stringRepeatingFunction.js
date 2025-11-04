321efa29349d98b18ad8c6e9493fada5a3f30586
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-04 14:33:43 -0600

    Finish stringRepeatingFunction

--- javaScript/buildAStringRepeatingFunction/stringRepeatingFunction.js
+++ javaScript/buildAStringRepeatingFunction/stringRepeatingFunction.js
@@ -1,1 +1,13 @@
- 
+function repeatStringNumTimes(str, num) {
+  if (num <= 0) {
+    return '';
+  }
+
+  let repeatedStr = '';
+
+  for (let i = 0; i < num; i++) {
+    repeatedStr += str;
+  }
+
+  return repeatedStr;
+}
