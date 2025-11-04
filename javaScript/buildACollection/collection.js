647c1cec45e3424993d5406df6b1f67bad59adb7
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-04 10:35:05 -0600

    Finish collection

--- javaScript/buildACollection/collection.js
+++ javaScript/buildACollection/collection.js
@@ -1,1 +1,43 @@
- 
+const bookCollection = {
+  2548: {
+    title: 'A book title',
+    author: 'An author name',
+    topics: ['a topic', 'another topic'],
+  },
+  2468: {
+    title: 'A different book title',
+    author: 'A different author name',
+    topics: ['a new topic', 'another new topic'],
+  },
+  1245: {
+    author: 'A name of an author',
+    topics: [],
+  },
+  5439: {
+    title: 'A title of a book',
+  },
+};
+
+function updateCollection(collection, id, prop, value) {
+  if (value === '') {
+    delete collection[id][prop];
+  }
+
+  if (prop !== 'topics' && value !== '') {
+    collection[id][prop] = value;
+  }
+
+  if (
+    prop === 'topics' &&
+    value !== '' &&
+    !collection[id].hasOwnProperty(prop)
+  ) {
+    collection[id][prop] = [];
+  }
+
+  if (prop === 'topics' && value !== '') {
+    collection[id][prop].push(value);
+  }
+
+  return collection;
+}
