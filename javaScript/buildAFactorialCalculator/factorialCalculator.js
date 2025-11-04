6a2429055f051d355b67dc0b1e1ecd8c07919343
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-04 10:54:04 -0600

    Finish factorialCalculator

--- javaScript/buildAFactorialCalculator/factorialCalculator.js
+++ javaScript/buildAFactorialCalculator/factorialCalculator.js
@@ -1,1 +1,15 @@
- 
+let num = 3;
+
+function factorialCalculator(num) {
+  let result = 1;
+
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
+}
+
+let factorial = factorialCalculator(num);
+let resultMsg = `Factorial of ${num} is ${factorial}`;
+console.log(resultMsg);
