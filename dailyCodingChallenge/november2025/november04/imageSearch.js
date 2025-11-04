55e2f0a94b48b650ac3e6a7d79645cbb3b87b8cb
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-04 08:20:48 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november04/imageSearch.js
+++ dailyCodingChallenge/november2025/november04/imageSearch.js
@@ -1,1 +1,24 @@
- 
+/*
+On November 4th, 2001, Google launched its image search, allowing people to find images 
+using search terms. In this challenge, you will imitate the image search.
+
+Given an array of image names and a search term, return an array of image names containing 
+the search term.
+
+Ignore the case when matching the search terms.
+Return the images in the same order they appear in the input array.
+*/
+
+/*
+1. imageSearch(["dog.png", "cat.jpg", "parrot.jpeg"], "dog") should return ["dog.png"].
+2. imageSearch(["Sunset.jpg", "Beach.png", "sunflower.jpeg"], "sun") should return 
+["Sunset.jpg", "sunflower.jpeg"].
+3. imageSearch(["Moon.png", "sun.jpeg", "stars.png"], "PNG") should return 
+["Moon.png", "stars.png"].
+4. imageSearch(["cat.jpg", "dogToy.jpeg", "kitty-cat.png", "catNip.jpeg", "franken_cat.gif"],
+"Cat") should return ["cat.jpg", "kitty-cat.png", "catNip.jpeg", "franken_cat.gif"].
+*/
+
+function imageSearch(images, term) {
+  return images;
+}
