2700fde37c463a86c3b4c1505913bd73c4981a74
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-04 10:44:57 -0600

    Finish largestWordFinder

--- javaScript/buildALongestWordFinderApp/largestWordFinder.js
+++ javaScript/buildALongestWordFinderApp/largestWordFinder.js
@@ -1,1 +1,12 @@
- 
+function findLongestWordLength(sentence) {
+  let words = sentence.split(' ');
+  let largestWord = '';
+
+  for (let word of words) {
+    if (word.length > largestWord.length) {
+      largestWord = word;
+    }
+  }
+
+  return largestWord.length;
+}
