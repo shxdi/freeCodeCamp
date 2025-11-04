a51b8e3b467d95f6f31d2188182bb8c24be0031c
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-04 14:09:32 -0600

    Finish chunkyMonkeyAlgorithm

--- javaScript/implementTheChunkyMonkeyAlgorithm/chunkyMonkeyAlgorithm.js
+++ javaScript/implementTheChunkyMonkeyAlgorithm/chunkyMonkeyAlgorithm.js
@@ -1,1 +1,16 @@
- 
+function chunkArrayInGroups(arr, num) {
+  let groupArr = [];
+
+  for (let i = 0; i < arr.length; i += num) {
+    let currentArr = [];
+
+    for (let j = 0 + i; j < num + i; j++) {
+      if (arr[j] !== undefined) {
+        currentArr.push(arr[j]);
+      }
+    }
+    groupArr.push(currentArr);
+  }
+
+  return groupArr;
+}
