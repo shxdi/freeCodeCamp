038c2ed6c1db86490762aefd724a2508e2f431e0
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-04 14:29:06 -0600

    Finish profileLookup

--- javaScript/buildAProfileLookup/profileLookup.js
+++ javaScript/buildAProfileLookup/profileLookup.js
@@ -1,1 +1,46 @@
- 
+let contacts = [
+  {
+    firstName: 'Akira',
+    lastName: 'Laine',
+    number: '0543236543',
+    likes: ['Pizza', 'Coding', 'Brownie Points'],
+  },
+  {
+    firstName: 'Harry',
+    lastName: 'Potter',
+    number: '0994372684',
+    likes: ['Hogwarts', 'Hagrid'],
+  },
+  {
+    firstName: 'Sherlock',
+    lastName: 'Holmes',
+    number: '0487345643',
+    likes: ['Intriguing Cases', 'Violin'],
+  },
+  {
+    firstName: 'Kristian',
+    lastName: 'Vos',
+    number: 'unknown',
+    likes: ['JavaScript', 'Gaming', 'Foxes'],
+  },
+];
+
+function lookUpProfile(name, property) {
+  let contact;
+
+  for (let person in contacts) {
+    if (contacts[person]['firstName'] === name) {
+      contact = contacts[person];
+    }
+  }
+
+  if (contact === undefined) {
+    return 'No such contact';
+  }
+
+  if (contact.hasOwnProperty(property)) {
+    return contact[property];
+  } else {
+    return 'No such property';
+  }
+}
