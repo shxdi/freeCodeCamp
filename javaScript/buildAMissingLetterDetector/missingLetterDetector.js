752db087a32ea36f71e6b923b381bbc340442e97
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-04 14:44:41 -0600

    Finish missingLetterDetector

--- javaScript/buildAMissingLetterDetector/missingLetterDetector.js
+++ javaScript/buildAMissingLetterDetector/missingLetterDetector.js
@@ -1,1 +1,14 @@
- 
+function fearNotLetter(str) {
+  str = str.toLowerCase();
+  let charCode = str.charCodeAt(0);
+
+  for (let i = 0; i < str.length; i++) {
+    let currentCharCode = str.charCodeAt(i);
+
+    if (charCode !== currentCharCode) {
+      return String.fromCharCode(charCode);
+    }
+
+    charCode++;
+  }
+}
