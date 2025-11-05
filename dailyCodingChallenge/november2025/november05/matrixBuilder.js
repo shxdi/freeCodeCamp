cf070f295e920000f7e8502cd97d2dec346fd839
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-05 07:30:32 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november05/matrixBuilder.js
+++ dailyCodingChallenge/november2025/november05/matrixBuilder.js
@@ -1,1 +1,22 @@
- 
+/*
+Given two integers (a number of rows and a number of columns), return a matrix (an array of 
+arrays) filled with zeros (0) of the given size.
+
+For example, given 2 and 3, return:
+
+[
+  [0, 0, 0],
+  [0, 0, 0]
+]
+*/
+
+/*
+1. buildMatrix(2, 3) should return [[0, 0, 0], [0, 0, 0]].
+2. buildMatrix(3, 2) should return [[0, 0], [0, 0], [0, 0]].
+3. buildMatrix(4, 3) should return [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]].
+4. buildMatrix(9, 1) should return [[0], [0], [0], [0], [0], [0], [0], [0], [0]].
+*/
+
+function buildMatrix(rows, cols) {
+  return rows;
+}
