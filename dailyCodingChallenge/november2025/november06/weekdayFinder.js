4687c177fa2b00720fca24ccdd1bd6e47e033d6f
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-06 09:13:33 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november06/weekdayFinder.js
+++ dailyCodingChallenge/november2025/november06/weekdayFinder.js
@@ -1,1 +1,26 @@
- 
+/*
+Given a string date in the format YYYY-MM-DD, return the day of the week
+
+Valid return days are:
+"Sunday"
+"Monday"
+"Tuesday"
+"Wednesday"
+"Thursday"
+"Friday"
+"Saturday"
+
+Be sure to ignore time zones
+*/
+
+/*
+1. getWeekday("2025-11-06") should return Thursday
+2. getWeekday("1999-12-31") should return Friday
+3. getWeekday("1111-11-11") should return Saturday
+4. getWeekday("2112-12-21") should return Wednesday
+5. getWeekday("2345-10-01") should return Monday
+*/
+
+function getWeekday(dateString) {
+  return dateString;
+}
