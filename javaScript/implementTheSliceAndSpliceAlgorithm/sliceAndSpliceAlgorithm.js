54ef14ce1d7ca7af44c0739a6999ceaba3c1db54
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-06 15:49:14 -0600

    Finish sliceAndSpliceAlgorithm

--- javaScript/implementTheSliceAndSpliceAlgorithm/sliceAndSpliceAlgorithm.js
+++ javaScript/implementTheSliceAndSpliceAlgorithm/sliceAndSpliceAlgorithm.js
@@ -1,1 +1,8 @@
- 
+function sliceAndSplice(arr1, arr2, index) {
+  let splicedArr = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    splicedArr.splice(index + i, 0, arr1[i]);
+  }
+
+  return splicedArr;
+}
