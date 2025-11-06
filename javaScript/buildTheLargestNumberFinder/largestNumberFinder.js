55c4d31c2ba5ea1eb0972234da442806e981881c
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-06 09:33:00 -0600

    Finish largestNumberFinder

--- javaScript/buildTheLargestNumberFinder/largestNumberFinder.js
+++ javaScript/buildTheLargestNumberFinder/largestNumberFinder.js
@@ -1,1 +1,19 @@
- 
+function largestOfAll(arr) {
+  let largestNumbers = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let largestNum;
+
+    for (let j = 0; j < arr[i].length; j++) {
+      if (j === 0) {
+        largestNum = arr[i][j];
+      } else if (largestNum < arr[i][j]) {
+        largestNum = arr[i][j];
+      }
+    }
+
+    largestNumbers.push(largestNum);
+  }
+
+  return largestNumbers;
+}
