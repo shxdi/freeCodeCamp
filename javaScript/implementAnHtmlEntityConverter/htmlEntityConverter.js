1d6d95d78a7e2d5e9be942828451407ce7c6a254
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-07 10:02:07 -0600

    Finish htmlEntityConverter

--- javaScript/implementAnHtmlEntityConverter/htmlEntityConverter.js
+++ javaScript/implementAnHtmlEntityConverter/htmlEntityConverter.js
@@ -1,1 +1,21 @@
- 
+function convertHTML(string) {
+  let convertedString = string.split('');
+
+  for (let i = 0; i < convertedString.length; i++) {
+    if (convertedString[i] === '&') {
+      convertedString[i] = '&amp;';
+    } else if (convertedString[i] === '<') {
+      convertedString[i] = '&lt;';
+    } else if (convertedString[i] === '>') {
+      convertedString[i] = '&gt;';
+    } else if (convertedString[i] === `"`) {
+      convertedString[i] = '&quot;';
+    } else if (convertedString[i] === `'`) {
+      convertedString[i] = '&apos;';
+    }
+  }
+
+  convertedString = convertedString.join('');
+
+  return convertedString;
+}
