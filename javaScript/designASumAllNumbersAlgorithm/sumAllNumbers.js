148c4a6983b355583554430208864e5f80650b24
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-07 10:54:17 -0600

    Finish sumAllNumbers

--- javaScript/designASumAllNumbersAlgorithm/sumAllNumbers.js
+++ javaScript/designASumAllNumbersAlgorithm/sumAllNumbers.js
@@ -1,1 +1,19 @@
- 
+function sumAll(numArr) {
+  let smallNum;
+  let bigNum;
+  let sum = 0;
+
+  if (numArr[0] > numArr[1]) {
+    bigNum = numArr[0];
+    smallNum = numArr[1];
+  } else {
+    bigNum = numArr[1];
+    smallNum = numArr[0];
+  }
+
+  for (let i = smallNum; i <= bigNum; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
