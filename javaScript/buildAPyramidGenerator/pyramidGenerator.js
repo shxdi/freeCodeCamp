5aab7f71bb85d0f6665c28d0aedf1a393633abf9
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-06 18:22:51 -0600

    Finish pyramidGenerator

--- javaScript/buildAPyramidGenerator/pyramidGenerator.js
+++ javaScript/buildAPyramidGenerator/pyramidGenerator.js
@@ -1,1 +1,26 @@
- 
+function pyramid(pattern, rows, direction) {
+  let pyramid = [];
+  let spacing = 1;
+  let rowSize = rows * 2 - 1;
+
+  pyramid.push(pattern.repeat(rowSize));
+
+  for (let i = 1; i < rows; i++) {
+    rowSize -= 2;
+    let newRow = ' '.repeat(spacing) + pattern.repeat(rowSize);
+
+    if (direction === true) {
+      pyramid.push(newRow);
+    } else {
+      pyramid.unshift(newRow);
+    }
+
+    spacing++;
+  }
+
+  // To make \n join at beginning and end
+  pyramid.push('');
+  pyramid.unshift('');
+
+  return pyramid.join('\n');
+}
