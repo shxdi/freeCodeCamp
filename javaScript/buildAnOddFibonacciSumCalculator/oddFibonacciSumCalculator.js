cc8f0a1d6c26e6b8001aa95ec1dbbc56c687c332
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-07 10:42:45 -0600

    Finish oddFibonacciSumCalculator

--- javaScript/buildAnOddFibonacciSumCalculator/oddFibonacciSumCalculator.js
+++ javaScript/buildAnOddFibonacciSumCalculator/oddFibonacciSumCalculator.js
@@ -1,1 +1,23 @@
- 
+function sumFibs(num) {
+  let sum = 1;
+  let newFib;
+
+  let fibOne = 0;
+  let fibTwo = 1;
+
+  while (fibOne + fibTwo <= num) {
+    newFib = fibOne + fibTwo;
+    console.log(newFib);
+
+    if (newFib % 2 !== 0) {
+      sum += newFib;
+    }
+
+    fibOne = fibTwo;
+    fibTwo = newFib;
+
+    console.log('sum:', sum);
+  }
+
+  return sum;
+}
