70c0d3dae0a4ae77da36a33615022e140311c8ad
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-07 09:53:48 -0600

    Finish dnaPairGenerator

--- javaScript/implementADNAPairGenerator/dnaPairGenerator.js
+++ javaScript/implementADNAPairGenerator/dnaPairGenerator.js
@@ -1,1 +1,17 @@
- 
+function pairElement(string) {
+  let dnaPairs = [];
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === 'A') {
+      dnaPairs.push(['A', 'T']);
+    } else if (string[i] === 'T') {
+      dnaPairs.push(['T', 'A']);
+    } else if (string[i] === 'C') {
+      dnaPairs.push(['C', 'G']);
+    } else if (string[i] === 'G') {
+      dnaPairs.push(['G', 'C']);
+    }
+  }
+
+  return dnaPairs;
+}
