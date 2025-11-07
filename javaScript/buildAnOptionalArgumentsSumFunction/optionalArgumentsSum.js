f2ea2d82b7c8f79b2303b51d97e6b4f3b7b012c1
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-07 16:08:24 -0600

    Finish optionalArgumentsSum

--- javaScript/buildAnOptionalArgumentsSumFunction/optionalArgumentsSum.js
+++ javaScript/buildAnOptionalArgumentsSumFunction/optionalArgumentsSum.js
@@ -1,1 +1,25 @@
- 
+function addTogether(...args) {
+  for (let arg of args) {
+    if (typeof arg !== 'number') {
+      return undefined;
+    }
+  }
+
+  if (args.length > 1) {
+    let sum = 0;
+
+    for (let arg of args) {
+      sum += arg;
+    }
+
+    return sum;
+  } else {
+    return function sumOf(num) {
+      if (typeof num !== 'number') {
+        return undefined;
+      }
+
+      return args[0] + num;
+    };
+  }
+}
