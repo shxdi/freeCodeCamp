f73f97bb8a14a3cfa22891b9de97fcc16a7b99ef
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-06 19:13:53 -0600

    Finish titleCaseConverter

--- javaScript/buildATitleCaseConverter/titleCaseConverter.js
+++ javaScript/buildATitleCaseConverter/titleCaseConverter.js
@@ -1,1 +1,9 @@
- 
+function titleCase(str) {
+  let words = str.split(' ');
+
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1).toLowerCase();
+  }
+
+  return words.join(' ');
+}
