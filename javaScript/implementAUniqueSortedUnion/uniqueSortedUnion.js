bb599081058e9cf5797ffbca08b103a99ceca01c
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-07 09:40:03 -0600

    Finish uniqueSortedUnion

--- javaScript/implementAUniqueSortedUnion/uniqueSortedUnion.js
+++ javaScript/implementAUniqueSortedUnion/uniqueSortedUnion.js
@@ -1,1 +1,13 @@
- 
+function uniteUnique(...args) {
+  let uniqueValues = [];
+
+  for (let arg of args) {
+    for (let i = 0; i < arg.length; i++) {
+      if (!uniqueValues.includes(arg[i])) {
+        uniqueValues.push(arg[i]);
+      }
+    }
+  }
+
+  return uniqueValues;
+}
