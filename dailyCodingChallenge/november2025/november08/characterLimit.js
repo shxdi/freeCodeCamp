db4d2a7199e4dbec51c5c5abe65ee640874accfc
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-08 08:52:06 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november08/characterLimit.js
+++ dailyCodingChallenge/november2025/november08/characterLimit.js
@@ -1,1 +1,20 @@
- 
+/*
+In this challenge, you are given a string and need to determine if it fits in a social media 
+post. Return the following strings based on the rules given:
+
+"short post" if it fits within a 40-character limit.
+"long post" if it's greater than 40 characters and fits within an 80-character limit.
+"invalid post" if it's too long to fit within either limit.
+*/
+
+/*
+1. canPost("Hello world") should return "short post".
+2. canPost("This is a longer message but still under eighty characters.") should return 
+"long post".
+3. canPost("This message is too long to fit into either of the character limits for a social 
+media post.") should return "invalid post".
+*/
+
+function canPost(message) {
+  return message;
+}
