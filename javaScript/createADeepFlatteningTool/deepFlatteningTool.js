4fe01629d1ca285143645160112fcaaef3a2f974
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-08 15:59:48 -0600

    Finish deepFlatteningTool

--- javaScript/createADeepFlatteningTool/deepFlatteningTool.js
+++ javaScript/createADeepFlatteningTool/deepFlatteningTool.js
@@ -1,1 +1,13 @@
- 
+function steamrollArray(arr) {
+  let flattenedArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flattenedArr = flattenedArr.concat(steamrollArray(arr[i]));
+    } else {
+      flattenedArr.push(arr[i]);
+    }
+  }
+
+  return flattenedArr;
+}
