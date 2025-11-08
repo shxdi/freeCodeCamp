539b4e6128d098a7f751977f2ad8ad96e346f424
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-08 15:42:55 -0600

    Finish rangeBasedLcmCalculator

--- javaScript/implementARangeBasedLcmCalculator/rangeBasedLcmCalculator.js
+++ javaScript/implementARangeBasedLcmCalculator/rangeBasedLcmCalculator.js
@@ -1,1 +1,22 @@
- 
+function smallestCommons(numArr) {
+  numArr.sort((a, b) => a - b);
+  let allBetween = [];
+  let leastCommon = 0;
+  let currentNum = numArr[1];
+
+  for (let i = numArr[0]; i <= numArr[1]; i++) {
+    allBetween.push(i);
+  }
+
+  while (leastCommon === 0) {
+    let testCurrent = allBetween.every((num) => currentNum % num === 0);
+
+    if (testCurrent) {
+      leastCommon = currentNum;
+    }
+
+    currentNum++;
+  }
+
+  return leastCommon;
+}
