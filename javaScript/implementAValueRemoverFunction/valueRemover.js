364e7d64764348a0c0a9687cdb1cbd354ece23e8
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-08 14:15:28 -0600

    Finish valueRemover

--- javaScript/implementAValueRemoverFunction/valueRemover.js
+++ javaScript/implementAValueRemoverFunction/valueRemover.js
@@ -1,1 +1,11 @@
- 
+function destroyer(arr, ...args) {
+  const finalArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!args.includes(arr[i])) {
+      finalArr.push(arr[i]);
+    }
+  }
+
+  return finalArr;
+}
