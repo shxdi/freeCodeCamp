9bd8ec1cfba5c35f5bb70340bf93349456949aba
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-08 14:54:22 -0600

    Add primeNumberSumCalculator.js

--- javaScript/buildAPrimeNumberSumCalculator/primeNumberSumCalculator.js
+++ javaScript/buildAPrimeNumberSumCalculator/primeNumberSumCalculator.js
@@ -1,1 +1,26 @@
- 
+function sumPrimes(num) {
+  if (num < 2) {
+    return 0;
+  }
+
+  let primeNumbers = [];
+  let currentNumber = 2;
+  let isPrime = true;
+
+  while (num >= currentNumber) {
+    for (let i = 2; i <= Math.sqrt(currentNumber); i++) {
+      if (currentNumber % i === 0) {
+        isPrime = false;
+      }
+    }
+
+    if (isPrime) {
+      primeNumbers.push(currentNumber);
+    }
+
+    currentNumber++;
+    isPrime = true;
+  }
+
+  return primeNumbers.reduce((accumulator, current) => accumulator + current);
+}
