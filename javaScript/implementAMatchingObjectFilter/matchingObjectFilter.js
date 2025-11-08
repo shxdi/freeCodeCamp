37b81876a4eb8013b21dfae9c339497fd61abd07
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-08 14:34:04 -0600

    Finish matchingObjectFilter

--- javaScript/implementAMatchingObjectFilter/matchingObjectFilter.js
+++ javaScript/implementAMatchingObjectFilter/matchingObjectFilter.js
@@ -1,1 +1,20 @@
- 
+function whatIsInAName(arr, obj) {
+  let foundPairs = [];
+  let testKeys = Object.keys(obj);
+
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+    let checksPassed = 0;
+    for (let j = 0; j < testKeys.length; j++) {
+      if (current[testKeys[j]] === obj[testKeys[j]]) {
+        checksPassed++;
+      }
+    }
+
+    if (checksPassed === testKeys.length) {
+      foundPairs.push(arr[i]);
+    }
+  }
+
+  return foundPairs;
+}
