0db3ba953736150af6d74c8bb202b0ccfbb5bf23
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-08 16:21:38 -0600

    Finish allTruePropertyValidator

--- javaScript/buildAnAllTruePropertyValidator/allTruePropertyValidator.js
+++ javaScript/buildAnAllTruePropertyValidator/allTruePropertyValidator.js
@@ -1,1 +1,9 @@
- 
+function truthCheck(arr, str) {
+  const testValues = [];
+
+  for (let ele of arr) {
+    testValues.push(ele[str]);
+  }
+
+  return testValues.every((value) => Boolean(value));
+}
