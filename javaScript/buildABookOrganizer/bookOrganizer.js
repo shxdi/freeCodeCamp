3e700387a80f20c0a5e6d66a71477062f1be8901
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-08 11:47:50 -0600

    Finish bookOrganizer

--- javaScript/buildABookOrganizer/bookOrganizer.js
+++ javaScript/buildABookOrganizer/bookOrganizer.js
@@ -1,1 +1,31 @@
- 
+const books = [
+  {
+    title: 'Book 1',
+    authorName: 'Author 1',
+    releaseYear: 2014,
+  },
+  {
+    title: 'Book 2',
+    authorName: 'Author 2',
+    releaseYear: 1932,
+  },
+  {
+    title: 'Book 3',
+    authorName: 'Author 1',
+    releaseYear: 1992,
+  },
+];
+
+function sortByYear(first, second) {
+  if (first.releaseYear < second.releaseYear) {
+    return -1;
+  } else if (first.releaseYear > second.releaseYear) {
+    return 1;
+  } else if (first.releaseYear === second.releaseYear) {
+    return 0;
+  }
+}
+
+let filteredBooks = books.filter((book) => book.releaseYear > 1950);
+
+filteredBooks.sort(sortByYear);
