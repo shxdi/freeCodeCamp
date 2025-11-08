005be6e1e98df2fe857b55a5b46f6f3f83542864
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-08 12:04:11 -0600

    Finish sortedIndexFinder

--- javaScript/implementASortedIndexFinder/sortedIndexFinder.js
+++ javaScript/implementASortedIndexFinder/sortedIndexFinder.js
@@ -1,1 +1,15 @@
- 
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => a - b);
+
+  const isLarger = (element) => element >= num;
+
+  let index = arr.findIndex(isLarger);
+
+  if (index === -1 && arr.length === 0) {
+    return 0;
+  } else if (index === -1) {
+    return arr.length;
+  } else {
+    return index;
+  }
+}
