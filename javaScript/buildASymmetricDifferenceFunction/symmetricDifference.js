bb549da4e99eb21d319789a8f53386701d19ee9b
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-08 13:05:27 -0600

    Finish symmetricDifference

--- javaScript/buildASymmetricDifferenceFunction/symmetricDifference.js
+++ javaScript/buildASymmetricDifferenceFunction/symmetricDifference.js
@@ -1,1 +1,26 @@
- 
+function diffArray(arrOne, arrTwo) {
+  let combinedArr;
+
+  if (arrOne.length === 0) {
+    combinedArr = arrTwo;
+  } else if (arrTwo.length === 0) {
+    combinedArr = arrOne;
+  } else {
+    combinedArr = `${arrOne},${arrTwo}`.split(',');
+  }
+
+  let differences = [];
+
+  differences = combinedArr.filter(checkIfDuplicate);
+
+  return differences;
+}
+
+function checkIfDuplicate(element, index, array) {
+  let firstIndex = array.indexOf(element);
+  let secondIndex = array.indexOf(element, firstIndex + 1);
+
+  if (secondIndex === -1) {
+    return true;
+  }
+}
