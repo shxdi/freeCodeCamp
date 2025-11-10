0e4db57ca0432de8c3b1f766833145373a6d5939
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-10 13:53:29 -0600

    Add seedArr

--- javaScript/buildAFilter/script.js
+++ javaScript/buildAFilter/script.js
@@ -1,1 +1,47 @@
- 
+const seedArr = [
+  {
+    category: 'vegetable',
+    type: 'Carrot',
+    typeClass: 'carrot',
+    name: 'Purple Dragon',
+  },
+  {
+    category: 'vegetable',
+    type: 'Pepper',
+    typeClass: 'pepper',
+    name: 'Sweet Moruga',
+  },
+  {
+    category: 'vegetable',
+    type: 'Lettuce',
+    typeClass: 'lettuce',
+    name: 'Spotted Aleppo Syrian',
+  },
+  { category: 'vegetable', type: 'Tomato', typeClass: 'tomato', name: 'Spoon' },
+  {
+    category: 'flower',
+    type: 'Cosmos',
+    typeClass: 'cosmos',
+    name: 'Apricot Lemonade',
+  },
+  {
+    category: 'flower',
+    type: 'Marigold',
+    typeClass: 'marigold',
+    name: 'Orange Flame',
+  },
+  {
+    category: 'flower',
+    type: 'Sunflower',
+    typeClass: 'sunflower',
+    name: 'Lemon Queen',
+  },
+  {
+    category: 'herb',
+    type: 'Catmint',
+    typeClass: 'catmint',
+    name: 'Grandview',
+  },
+  { category: 'herb', type: 'Bee Balm', typeClass: 'bee-balm', name: 'Lemon' },
+  { category: 'herb', type: 'Basil', typeClass: 'basil', name: 'Persian' },
+];
