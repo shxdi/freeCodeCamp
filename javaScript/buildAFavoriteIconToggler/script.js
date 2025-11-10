a8fdb4478d52c330d81e43b1b39dd81e744fbec0
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-10 10:28:18 -0600

    Add function to buttons

--- javaScript/buildAFavoriteIconToggler/script.js
+++ javaScript/buildAFavoriteIconToggler/script.js
@@ -1,1 +1,13 @@
- 
+const btns = document.querySelectorAll('.favorite-icon');
+
+btns.forEach((btn) => btn.addEventListener('click', () => toggleBtn(btn)));
+
+function toggleBtn(btn) {
+  if (btn.className.includes('filled')) {
+    btn.classList.remove('filled');
+    btn.innerHTML = '&#9825';
+  } else {
+    btn.classList.add('filled');
+    btn.innerHTML = '&#10084';
+  }
+}
