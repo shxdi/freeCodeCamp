0163150e57ad5d34ef7b5c6ff7f4445d43564244
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-10 17:07:01 -0600

    Add querySelectors and eventListener

--- javaScript/buildALightboxViewer/script.js
+++ javaScript/buildALightboxViewer/script.js
@@ -1,1 +1,9 @@
- 
+const gallery = document.querySelector('.gallery');
+const lightbox = document.querySelector('.lightbox');
+const lightboxImg = document.querySelector('.lightbox-img');
+
+gallery.addEventListener('click', (event) => {
+  if (event.target.classList.contains('gallery-item')) {
+    lightbox.classList.add('show');
+  }
+});
