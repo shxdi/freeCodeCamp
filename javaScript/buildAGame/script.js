7f1a8aabbad48de8095b495621ebc4f8d91ddb16
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-11 10:59:11 -0600

    Add selectors and eventListener for selecting num

--- javaScript/buildAGame/script.js
+++ javaScript/buildAGame/script.js
@@ -1,1 +1,20 @@
- 
+const availableNums = document.querySelector('.available-nums');
+const numContainers = document.querySelectorAll('.available-nums div');
+
+let selectedNum;
+
+availableNums.addEventListener('click', (event) => {
+  selectNum(event);
+});
+
+function selectNum(event) {
+  selectedNum = event.target.textContent;
+
+  numContainers.forEach((element) => {
+    if (element.classList.contains('selected')) {
+      element.removeAttribute('class');
+    }
+  });
+
+  event.target.classList.add('selected');
+}
