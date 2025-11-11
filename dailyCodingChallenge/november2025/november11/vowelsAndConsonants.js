dec645d9bed3e501ba9033a863028cf6d41daa2f
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-11 08:49:32 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november11/vowelsAndConsonants.js
+++ dailyCodingChallenge/november2025/november11/vowelsAndConsonants.js
@@ -1,1 +1,22 @@
- 
+/*
+Given a string, return an array with the number of vowels and number of consonants in the 
+string.
+
+Vowels consist of a, e, i, o, u in any case.
+Consonants consist of all other letters in any case.
+Ignore any non-letter characters.
+For example, given "Hello World", return [3, 7].
+*/
+
+/*
+1. count("Hello World") should return [3, 7].
+2. count("JavaScript") should return [3, 7].
+3. count("Python") should return [1, 5].
+4. count("freeCodeCamp") should return [5, 7].
+5. count("Hello, World!") should return [3, 7].
+6. count("The quick brown fox jumps over the lazy dog.") should return [11, 24].
+*/
+
+function count(str) {
+  return str;
+}
