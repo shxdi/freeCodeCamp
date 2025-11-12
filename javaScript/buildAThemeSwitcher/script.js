e14e747d4855cfa44cd8058c03abfc32187152f4
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-12 11:26:26 -0600

    Add themeArr

--- javaScript/buildAThemeSwitcher/script.js
+++ javaScript/buildAThemeSwitcher/script.js
@@ -1,1 +1,5 @@
- 
+const themesArr = [
+  { name: 'preferred', message: 'This is your preferred theme' },
+  { name: 'light', message: 'This is the light theme' },
+  { name: 'dark', message: 'This is the dark theme' },
+];
