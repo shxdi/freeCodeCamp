ede554df4e2292b09eb24b8d39267b749eaaf9c5
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-12 11:26:53 -0600

    Add eventListener to #theme-switcher-button

--- javaScript/buildAThemeSwitcher/script.js
+++ javaScript/buildAThemeSwitcher/script.js
@@ -3,3 +3,18 @@ const themesArr = [
   { name: 'light', message: 'This is the light theme' },
   { name: 'dark', message: 'This is the dark theme' },
 ];
+
+const themeBtn = document.getElementById('theme-switcher-button');
+const themeDropdown = document.getElementById('theme-dropdown');
+
+themeBtn.addEventListener('click', () => {
+  let isExpanded = themeBtn.getAttribute('aria-expanded');
+
+  if (isExpanded === 'true') {
+    themeBtn.setAttribute('aria-expanded', 'false');
+    themeDropdown.toggleAttribute('hidden');
+  } else {
+    themeBtn.setAttribute('aria-expanded', 'true');
+    themeDropdown.toggleAttribute('hidden');
+  }
+});
