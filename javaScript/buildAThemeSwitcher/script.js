cce8776b0b6f26778471dd9b62561b29418c0496
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-12 12:22:49 -0600

    Change themesArr to themes

--- javaScript/buildAThemeSwitcher/script.js
+++ javaScript/buildAThemeSwitcher/script.js
@@ -1,4 +1,4 @@
-const themesArr = [
+const themes = [
   { name: 'preferred', message: 'This is your preferred theme.' },
   { name: 'light', message: 'This is the light theme.' },
   { name: 'dark', message: 'This is the dark theme.' },
