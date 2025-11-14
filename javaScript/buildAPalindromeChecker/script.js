ed8c08ee00d65a1fc3b4398417c9ef9f773b8ba6
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-14 10:59:25 -0600

    Finish buildAPalindromeChecker

--- javaScript/buildAPalindromeChecker/script.js
+++ javaScript/buildAPalindromeChecker/script.js
@@ -1,1 +1,33 @@
- 
+const button = document.getElementById('check-btn');
+const textInput = document.getElementById('text-input');
+const result = document.getElementById('result');
+
+button.addEventListener('click', () => {
+  let input = textInput.value.toLowerCase();
+
+  if (input === '') {
+    alert('Please input a value');
+  }
+
+  let strForwards = removeNonWordChar(input);
+  let strBackwards = strForwards.split('').reverse().join('');
+
+  if (strForwards === strBackwards) {
+    result.innerHTML = `${input} is a palindrome`;
+  } else {
+    result.innerHTML = `${input} is not a palindrome`;
+  }
+});
+
+function removeNonWordChar(str) {
+  let reducedInput = '';
+  let regex = /[a-z0-9]/i;
+
+  for (let char of str) {
+    if (regex.test(char)) {
+      reducedInput += char;
+    }
+  }
+
+  return reducedInput;
+}
