3f9009774f2c34624c7b2474b49c321d2c2939e6
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-14 14:51:05 -0600

    Finish smartWordReplacement

--- javaScript/buildASmartWordReplacementFunction/smartWordReplacement.js
+++ javaScript/buildASmartWordReplacementFunction/smartWordReplacement.js
@@ -1,1 +1,10 @@
- 
+function myReplace(str, word, sub) {
+  if (word[0].toUpperCase() === word[0]) {
+    sub = sub[0].toUpperCase() + sub.slice(1);
+    console.log(sub);
+  } else {
+    sub = sub[0].toLowerCase() + sub.slice(1);
+    console.log(sub);
+  }
+  return str.replace(word, sub);
+}
