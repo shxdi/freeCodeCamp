5a6caeda0ffd8129a676b87cb62827955edabe86
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-14 14:18:45 -0600

    Finish spinalCaseConverter

--- javaScript/implementASpinalCaseConverter/spinalCaseConverter.js
+++ javaScript/implementASpinalCaseConverter/spinalCaseConverter.js
@@ -1,1 +1,8 @@
- 
+function spinalCase(str) {
+  let seperatedStr = str.replace(/([a-z0-9])([A-Z])/g, '$1 $2');
+
+  let regex = /[\s_]/g;
+  let spinal = seperatedStr.replace(regex, '-');
+
+  return spinal.toLowerCase();
+}
