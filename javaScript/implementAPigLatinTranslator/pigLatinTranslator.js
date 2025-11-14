f51a6a2e9f807fb49e50c673241a0caf327a95ac
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-14 14:39:09 -0600

    Finish pigLatinTranslator

--- javaScript/implementAPigLatinTranslator/pigLatinTranslator.js
+++ javaScript/implementAPigLatinTranslator/pigLatinTranslator.js
@@ -1,1 +1,14 @@
- 
+function translatePigLatin(str) {
+  let startsWithVowel = /^[aeiou]/.test(str);
+  let hasVowel = /[aeiou]/g.test(str);
+
+  if (startsWithVowel) {
+    return str + 'way';
+  }
+
+  if (!hasVowel) {
+    return str + 'ay';
+  }
+
+  return str.replace(/([^aeiou]+)(\w+)/, '$2$1ay');
+}
