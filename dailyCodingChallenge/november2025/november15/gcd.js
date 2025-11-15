8c1b70356342821efef60c532250f575d0400f2a
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-15 08:13:05 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november15/gcd.js
+++ dailyCodingChallenge/november2025/november15/gcd.js
@@ -1,1 +1,20 @@
- 
+/*
+Given two positive integers, return their greatest common divisor (GCD).
+
+The GCD of two integers is the largest number that divides evenly into both numbers without 
+leaving a remainder.
+For example, the divisors of 4 are 1, 2, and 4. The divisors of 6 are 1, 2, 3, and 6. So given 
+4 and 6, return 2, the largest number that appears in both sets of divisors.
+*/
+
+/*
+1. gcd(4, 6) should return 2.
+2. gcd(20, 15) should return 5.
+3. gcd(13, 17) should return 1.
+4. gcd(654, 456) should return 6.
+5. gcd(3456, 4320) should return 864.
+*/
+
+function gcd(x, y) {
+  return x;
+}
