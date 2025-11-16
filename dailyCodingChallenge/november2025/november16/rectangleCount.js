018e0cfb47da8e1ef735980f2f2a43c423f10058
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-16 08:11:06 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november16/rectangleCount.js
+++ dailyCodingChallenge/november2025/november16/rectangleCount.js
@@ -1,1 +1,20 @@
- 
+/*
+Given two positive integers representing the width and height of a rectangle, determine how 
+many rectangles can fit in the given one.
+
+Only count rectangles with integer width and height.
+For example, given 1 and 3, return 6. Three 1x1 rectangles, two 1x2 rectangles, and one 1x3 
+rectangle.
+*/
+
+/*
+1. countRectangles(1, 3) should return 6.
+2. countRectangles(3, 2) should return 18.
+3. countRectangles(1, 2) should return 3.
+4. countRectangles(5, 4) should return 150.
+5. countRectangles(11, 19) should return 12540.
+*/
+
+function countRectangles(width, height) {
+  return width;
+}
