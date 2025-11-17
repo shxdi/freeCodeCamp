66af7bd79cf3805f1729f67cf77b301d760f048c
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-17 14:31:02 -0600

    Finish dateConversion

--- javaScript/buildADateConversionProgram/dateConversion.js
+++ javaScript/buildADateConversionProgram/dateConversion.js
@@ -1,1 +1,19 @@
- 
+let currentDate = new Date();
+let currentDateFormat = `Current Date and Time: ${currentDate}`;
+
+console.log(currentDateFormat);
+
+function formatDateMMDDYYYY(date) {
+  let formattedDate = date.toLocaleDateString('en-US');
+
+  return `Formatted Date (MM/DD/YYYY): ${formattedDate}`;
+}
+
+function formatDateLong(date) {
+  const options = {
+    month: 'long',
+  };
+  let formattedDate = date.toLocaleDateString('en-US', options);
+
+  return `Formatted Date (Month Day, Year): ${formattedDate}`;
+}
