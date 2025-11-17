bffaf7659da068895b39cd5c7bd9dbd67d28689e
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-17 08:44:59 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november17/fingerprintTest.js
+++ dailyCodingChallenge/november2025/november17/fingerprintTest.js
@@ -1,1 +1,25 @@
- 
+/*
+Given two strings representing fingerprints, determine if they are a match using the following 
+rules:
+
+Each fingerprint will consist only of lowercase letters (a-z).
+Two fingerprints are considered a match if:
+They are the same length.
+The number of differing characters does not exceed 10% of the fingerprint length.
+*/
+
+/*
+1. isMatch("helloworld", "helloworld") should return true.
+2. isMatch("helloworld", "helloworlds") should return false.
+3. isMatch("helloworld", "jelloworld") should return true.
+4. isMatch("thequickbrownfoxjumpsoverthelazydog", "thequickbrownfoxjumpsoverthelazydog") 
+should return true.
+5. isMatch("theslickbrownfoxjumpsoverthelazydog", "thequickbrownfoxjumpsoverthehazydog") 
+should return true.
+6. isMatch("thequickbrownfoxjumpsoverthelazydog", "thequickbrownfoxjumpsoverthehazycat") 
+should return false.
+*/
+
+function isMatch(fingerprintA, fingerprintB) {
+  return fingerprintA;
+}
