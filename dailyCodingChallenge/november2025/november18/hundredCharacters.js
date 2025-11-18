7b2f3d5d57d5e1dd107aa8916ce5ef33f15020fd
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-18 07:28:12 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november18/hundredCharacters.js
+++ dailyCodingChallenge/november2025/november18/hundredCharacters.js
@@ -1,1 +1,21 @@
- 
+/*
+Welcome to the 100th Daily Coding Challenge!
+
+Given a string, repeat its characters until the result is exactly 100 characters long. 
+If your repetitions go over 100 characters, trim the extra so it's exactly 100.
+*/
+
+/*
+1. oneHundred("One hundred ") should return "One hundred One hundred One hundred One hundred One 
+hundred One hundred One hundred One hundred One ".
+2. oneHundred("freeCodeCamp ") should return "freeCodeCamp freeCodeCamp freeCodeCamp 
+freeCodeCamp freeCodeCamp freeCodeCamp freeCodeCamp freeCodeC".
+3. oneHundred("daily challenges ") should return "daily challenges daily challenges daily 
+challenges daily challenges daily challenges daily challenge".
+4. oneHundred("!") should return "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".
+*/
+
+function oneHundred(chars) {
+  return chars;
+}
