45f77d8be5b5ed0bf8077a17a66882cb726b24fb
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-20 08:43:55 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november20/longestWord.js
+++ dailyCodingChallenge/november2025/november20/longestWord.js
@@ -1,1 +1,22 @@
- 
+/*
+Given a sentence string, return the longest word in the sentence.
+
+Words are separated by a single space.
+Only letters (a-z, case-insensitive) count toward the word's length.
+If there are multiple words with the same length, return the first one that appears.
+Return the word as it appears in the given string, with punctuation removed.
+*/
+
+/*
+1. longestWord("The quick red fox") should return "quick".
+2. longestWord("Hello coding challenge.") should return "challenge".
+3. longestWord("Do Try This At Home.") should return "This".
+4. longestWord("This sentence... has commas, ellipses, and an exlamation point!") should 
+    return "exlamation".
+5. longestWord("A tie? No way!") should return "tie".
+6. longestWord("Wouldn't you like to know.") should return "Wouldnt".
+*/
+
+function longestWord(sentence) {
+  return sentence;
+}
