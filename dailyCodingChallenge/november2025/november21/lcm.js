9ced259bef3d43c1428c2686ec34c9d457b7b338
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-21 08:34:05 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november21/lcm.js
+++ dailyCodingChallenge/november2025/november21/lcm.js
@@ -1,1 +1,22 @@
- 
+/*
+Given two integers, return the least common multiple (LCM) of the two numbers.
+
+The LCM of two numbers is the smallest positive integer that is a multiple of both numbers. 
+For example, given 4 and 6, return 12 because:
+
+Multiples of 4 are 4, 8, 12 and so on.
+Multplies of 6 are 6, 12, 18 and so on.
+12 is the smallest number that is a multiple of both.
+*/
+
+/*
+1. lcm(4, 6) should return 12.
+2. lcm(9, 6) should return 18.
+3. lcm(10, 100) should return 100.
+4. lcm(13, 17) should return 221.
+5. lcm(45, 70) should return 630.
+*/
+
+function lcm(a, b) {
+  return a;
+}
