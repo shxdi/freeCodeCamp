65de5852b563d7b4337d61c96e21209bc04ef5bb
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-25 09:20:12 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november24/messageValidator.js
+++ dailyCodingChallenge/november2025/november24/messageValidator.js
@@ -1,1 +1,22 @@
- 
+/*
+Given a message string and a validation string, determine if the message is valid.
+
+A message is valid if each word in the message starts with the corresponding letter in the 
+    validation string, in order.
+Letters are case-insensitive.
+Words in the message are separated by single spaces.
+*/
+
+/*
+1. isValidMessage("hello world", "hw") should return true.
+2. isValidMessage("ALL CAPITAL LETTERS", "acl") should return true.
+3. isValidMessage("Coding challenge are boring.", "cca") should return false.
+4. isValidMessage("The quick brown fox jumps over the lazy dog.", "TQBFJOTLD") should 
+    return true.
+5. isValidMessage("The quick brown fox jumps over the lazy dog.", "TQBFJOTLDT") should 
+    return false.
+*/
+
+function isValidMessage(message, validator) {
+  return message;
+}
