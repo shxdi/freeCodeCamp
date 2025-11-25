1b3470c93a68d6257790afedf227dedcd554b51e
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-25 10:09:40 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november25/fizzBuzz.js
+++ dailyCodingChallenge/november2025/november25/fizzBuzz.js
@@ -1,1 +1,24 @@
- 
+/*
+Given an integer (n), return an array of integers from 1 to n (inclusive), replacing numbers 
+    that are multiple of:
+
+3 with "Fizz".
+5 with "Buzz".
+3 and 5 with "FizzBuzz".
+*/
+
+/*
+1. fizzBuzz(2) should return [1, 2].
+2. fizzBuzz(4) should return [1, 2, "Fizz", 4].
+3. fizzBuzz(8) should return [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8].
+4. fizzBuzz(20) should return [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, 
+    "Fizz", 13, 14, "FizzBuzz", 16, 17, "Fizz", 19, "Buzz"].
+5. fizzBuzz(50) should return [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, 
+    "Fizz", 13, 14, "FizzBuzz", 16, 17, "Fizz", 19, "Buzz", "Fizz", 22, 23, "Fizz", "Buzz", 
+    26, "Fizz", 28, 29, "FizzBuzz", 31, 32, "Fizz", 34, "Buzz", "Fizz", 37, 38, "Fizz", 
+    "Buzz", 41, "Fizz", 43, 44, "FizzBuzz", 46, 47, "Fizz", 49, "Buzz"].
+*/
+
+function fizzBuzz(n) {
+  return n;
+}
