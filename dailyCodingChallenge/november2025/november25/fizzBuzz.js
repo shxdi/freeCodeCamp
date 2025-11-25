3f81add3fb2a95c90d36d1f24cb9a2023aa6d0fd
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-25 10:12:59 -0600

    Finish fizzBuzz

--- dailyCodingChallenge/november2025/november25/fizzBuzz.js
+++ dailyCodingChallenge/november2025/november25/fizzBuzz.js
@@ -20,5 +20,19 @@ Given an integer (n), return an array of integers from 1 to n (inclusive), repla
 */
 
 function fizzBuzz(n) {
+  const fizzBuzzArr = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      fizzBuzzArr.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      fizzBuzzArr.push('Fizz');
+    } else if (i % 5 === 0) {
+      fizzBuzzArr.push('Buzz');
+    } else {
+      fizzBuzzArr.push(i);
+    }
+  }
+
   return n;
 }
