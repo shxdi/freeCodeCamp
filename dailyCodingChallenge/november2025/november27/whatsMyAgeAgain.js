6309e7478c4f870157d5411ee4b104f0cf3a0ccb
shxdi <234461025+shxdi@users.noreply.github.com>
2025-11-27 09:41:11 -0600

    Add provided information

--- dailyCodingChallenge/november2025/november27/whatsMyAgeAgain.js
+++ dailyCodingChallenge/november2025/november27/whatsMyAgeAgain.js
@@ -1,1 +1,20 @@
- 
+/*
+Given the date of someone's birthday in the format YYYY-MM-DD, return the person's age as of 
+    November 27th, 2025.
+
+Assume all birthdays are valid dates before November 27th, 2025.
+Return the age as an integer.
+Be sure to account for whether the person has already had their birthday in 2025.
+*/
+
+/*
+1. calculateAge("2000-11-20") should return 25.
+2. calculateAge("2000-12-01") should return 24.
+3. calculateAge("2014-10-25") should return 11.
+4. calculateAge("1994-01-06") should return 31.
+5. calculateAge("1994-12-14") should return 30.
+*/
+
+function calculateAge(birthday) {
+  return birthday;
+}
