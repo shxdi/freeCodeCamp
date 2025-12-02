1d57417a8145f852bb0d41d1ce87c11924074e3b
shxdi <234461025+shxdi@users.noreply.github.com>
2025-12-02 07:54:28 -0600

    Add provided information

--- dailyCodingChallenge/december2025/december02/camelToSnake.js
+++ dailyCodingChallenge/december2025/december02/camelToSnake.js
@@ -1,1 +1,20 @@
- 
+/*
+Given a string in camel case, return the snake case version of the string using the following 
+    rules:
+
+The input string will contain only letters (A-Z and a-z) and will always start with a lowercase 
+    letter.
+Every uppercase letter in the camel case string starts a new word.
+Convert all letters to lowercase.
+Separate words with an underscore (_).
+*/
+
+/*
+1. toSnake("helloWorld") should return "hello_world".
+2. toSnake("myVariableName") should return "my_variable_name".
+3. toSnake("freecodecampDailyChallenges") should return "freecodecamp_daily_challenges".
+*/
+
+function toSnake(camel) {
+  return camel;
+}
