7909cdbfb6db81c327d831b207dd6687f3fd54d0
shxdi <234461025+shxdi@users.noreply.github.com>
2025-12-02 12:11:35 -0600

    Finish buildAPermutationGenerator

--- javaScript/buildAPermutationGenerator/script.js
+++ javaScript/buildAPermutationGenerator/script.js
@@ -1,1 +1,25 @@
- 
+function permuteString(str, prefix = '', arr = []) {
+  if (str.length === 0) {
+    arr.push(prefix);
+    prefix = '';
+    return arr;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let currentChar = prefix;
+    currentChar += str[i];
+    let remainingChars = str.slice(0, i) + str.slice(i + 1);
+
+    permuteString(remainingChars, currentChar, arr);
+  }
+
+  // Remove duplicates
+  let results = [];
+  arr.forEach((item) => {
+    if (!results.includes(item)) {
+      results.push(item);
+    }
+  });
+
+  return results;
+}
