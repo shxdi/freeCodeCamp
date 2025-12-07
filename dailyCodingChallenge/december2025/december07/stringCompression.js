bae7a5b42b5532a60e1b75b0688b4b40c69e87c1
shxdi <234461025+shxdi@users.noreply.github.com>
2025-12-07 08:03:37 -0600

    Add provided information

--- dailyCodingChallenge/december2025/december07/stringCompression.js
+++ dailyCodingChallenge/december2025/december07/stringCompression.js
@@ -1,1 +1,22 @@
- 
+/*
+Given a string sentence, return a compressed version of the sentence where consecutive 
+    duplicate words are replaced by the word followed with the number of times it repeats 
+    in parentheses.
+
+Only consecutive duplicates are compressed.
+Words are separated by single spaces.
+For example, given "yes yes yes please", return "yes(3) please".
+*/
+
+/*
+1. compressString("yes yes yes please") should return "yes(3) please".
+2. compressString("I have have have apples") should return "I have(3) apples".
+3. compressString("one one three and to the the the the") should return "one(2) three and to 
+    the(4)".
+4. compressString("route route route route route route tee tee tee tee tee tee") should return 
+    "route(6) tee(6)".
+*/
+
+function compressString(sentence) {
+  return sentence;
+}
