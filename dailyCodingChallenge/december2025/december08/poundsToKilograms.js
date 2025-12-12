a610dd796ffc4d4ca2bb7e47d42dbb54e6db261a
shxdi <234461025+shxdi@users.noreply.github.com>
2025-12-12 08:00:31 -0600

    Add provided information

--- dailyCodingChallenge/december2025/december08/poundsToKilograms.js
+++ dailyCodingChallenge/december2025/december08/poundsToKilograms.js
@@ -1,1 +1,22 @@
- 
+/*
+Given a weight in pounds as a number, return the string "(lbs) pounds equals (kgs) kilograms.".
+
+Replace "(lbs)" with the input number.
+Replace "(kgs)" with the input converted to kilograms, rounded to two decimals and always 
+    include two decimal places in the value.
+1 pound equals 0.453592 kilograms.
+If the input is 1, use "pound" instead of "pounds".
+If the converted value is 1, use "kilogram" instead of "kilograms".
+*/
+
+/*
+1. convertToKgs(1) should return "1 pound equals 0.45 kilograms.".
+2. convertToKgs(0) should return "0 pounds equals 0.00 kilograms.".
+3. convertToKgs(100) should return "100 pounds equals 45.36 kilograms.".
+4. convertToKgs(2.5) should return "2.5 pounds equals 1.13 kilograms.".
+5. convertToKgs(2.20462) should return "2.20462 pounds equals 1.00 kilogram.".
+*/
+
+function convertToKgs(lbs) {
+  return lbs;
+}
