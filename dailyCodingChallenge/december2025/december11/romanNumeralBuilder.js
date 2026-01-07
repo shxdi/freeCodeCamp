38e0493d11897f04cfb1a0cf204cae3e6bbbcb5e
shxdi <234461025+shxdi@users.noreply.github.com>
2026-01-07 13:14:15 -0600

    Finish romanNumeralBuilder

--- dailyCodingChallenge/december2025/december11/romanNumeralBuilder.js
+++ dailyCodingChallenge/december2025/december11/romanNumeralBuilder.js
@@ -34,5 +34,56 @@ Here's one more example: given 1464, return "MCDLXIV" (1000 + (500 - 100) + 50 +
 */
 
 function toRoman(num) {
-  return num;
+  let remaining = num;
+  let roman = '';
+
+  if (num > 3999) {
+    return 'Number is too large';
+  }
+
+  while (remaining > 0) {
+    if (remaining >= 1000) {
+      roman += 'M';
+      remaining -= 1000;
+    } else if (remaining >= 900) {
+      roman += 'CM';
+      remaining -= 900;
+    } else if (remaining >= 500) {
+      roman += 'D';
+      remaining -= 500;
+    } else if (remaining >= 400) {
+      roman += 'CD';
+      remaining -= 400;
+    } else if (remaining >= 100) {
+      roman += 'C';
+      remaining -= 100;
+    } else if (remaining >= 90) {
+      roman += 'XC';
+      remaining -= 90;
+    } else if (remaining >= 50) {
+      roman += 'L';
+      remaining -= 50;
+    } else if (remaining >= 40) {
+      roman += 'XL';
+      remaining -= 40;
+    } else if (remaining >= 10) {
+      roman += 'X';
+      remaining -= 10;
+    } else if (remaining >= 9) {
+      roman += 'IX';
+      remaining -= 9;
+    } else if (remaining >= 5) {
+      roman += 'V';
+      remaining -= 5;
+    } else if (remaining >= 4) {
+      roman += 'IV';
+      remaining -= 4;
+    } else if (remaining >= 1) {
+      roman += 'I';
+      remaining -= 1;
+    }
+  }
+
+  console.log(remaining);
+  return roman;
 }
