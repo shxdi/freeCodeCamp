2ac928a0bc5718c7813933de3a9178b597b744dc
shxdi <234461025+shxdi@users.noreply.github.com>
2026-01-09 14:58:54 -0600

    Add provided information

--- dailyCodingChallenge/december2025/december17/markdownBlockquoteParser.js
+++ dailyCodingChallenge/december2025/december17/markdownBlockquoteParser.js
@@ -1,1 +1,29 @@
- 
+/*
+Given a string that includes a blockquote in Markdown, return the equivalent HTML string.
+
+A blockquote in Markdown is any line that:
+
+Starts with zero or more spaces
+Followed by a greater-than sign (>)
+Then, one or more spaces
+And finally, the blockquote text.
+Return the blockquote text surrounded by opening and closing HTML blockquote tags.
+
+For example, given "> This is a quote", return <blockquote>This is a quote</blockquote>.
+
+Note: The console may not display HTML tags in strings when logging messages. Check the 
+    browser console to see logs with tags included.
+*/
+
+/*
+1. parseBlockquote("> This is a quote") should 
+    return "<blockquote>This is a quote</blockquote>".
+2. parseBlockquote(" > This is also a quote") should 
+    return "<blockquote>This is also a quote</blockquote>".
+3. parseBlockquote("  >    So  Is  This") should 
+    return "<blockquote>So  Is  This</blockquote>".
+*/
+
+function parseBlockquote(markdown) {
+  return markdown;
+}
