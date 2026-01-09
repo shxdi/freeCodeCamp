9d8500e1cbcae85b11bff64e35e08ae7b593df3a
shxdi <234461025+shxdi@users.noreply.github.com>
2026-01-09 15:34:43 -0600

    Add provided information

--- dailyCodingChallenge/december2025/december20/purgeMostFrequent.js
+++ dailyCodingChallenge/december2025/december20/purgeMostFrequent.js
@@ -1,1 +1,20 @@
- 
+/*
+Given an array of values, remove all occurrences of the most frequently occurring element 
+    and return the resulting array.
+
+If multiple values are tied for most frequent, remove all of them.
+Do not change any of the other elements or their order.
+*/
+
+/*
+1. purgeMostFrequent([1, 2, 2, 3]) should return [1, 3].
+2. purgeMostFrequent(["a", "b", "d", "b", "c", "d", "c", "d", "c", "d"]) should 
+    return ["a", "b", "b", "c", "c", "c"].
+3. purgeMostFrequent(["red", "blue", "green", "red", "blue", "green", "blue"]) should 
+    return ["red", "green", "red", "green"].
+4. purgeMostFrequent([5, 5, 5, 5]) should return [].
+*/
+
+function purgeMostFrequent(arr) {
+  return arr;
+}
