d782b476d59c66a2ed74503a373e6546cbff6057
shxdi <234461025+shxdi@users.noreply.github.com>
2026-01-09 08:42:47 -0600

    Add provided information

--- dailyCodingChallenge/december2025/december14/capitalizeIt.js
+++ dailyCodingChallenge/december2025/december14/capitalizeIt.js
@@ -1,1 +1,18 @@
- 
+/*
+Given a string title, return a new string formatted in title case using the following rules:
+
+Capitalize the first letter of each word.
+Make all other letters in each word lowercase.
+Words are always separated by a single space.
+*/
+
+/*
+1. titleCase("hello world") should return "Hello World".
+2. titleCase("the quick brown fox") should return "The Quick Brown Fox".
+3. titleCase("JAVASCRIPT AND PYTHON") should return "Javascript And Python".
+4. titleCase("AvOcAdO tOAst fOr brEAkfAst") should return "Avocado Toast For Breakfast".
+*/
+
+function titleCase(title) {
+  return title;
+}
