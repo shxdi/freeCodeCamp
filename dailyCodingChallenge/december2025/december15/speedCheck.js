8c2e9546e24426689b97e46f7704b01360870d12
shxdi <234461025+shxdi@users.noreply.github.com>
2026-01-09 08:53:38 -0600

    Add provided information

--- dailyCodingChallenge/december2025/december15/speedCheck.js
+++ dailyCodingChallenge/december2025/december15/speedCheck.js
@@ -1,1 +1,22 @@
- 
+/*
+Given the speed you are traveling in miles per hour (MPH), and a speed limit in kilometers per 
+    hour (KPH), determine whether you are speeding and if you will get a warning or a ticket.
+
+1 mile equals 1.60934 kilometers.
+If you are traveling less than or equal to the speed limit, return "Not Speeding".
+If you are traveling 5 KPH or less over the speed limit, return "Warning".
+If you are traveling more than 5 KPH over the speed limit, return "Ticket".
+*/
+
+/*
+1. speedCheck(30, 70) should return "Not Speeding".
+2. speedCheck(40, 60) should return "Warning".
+3. speedCheck(40, 65) should return "Not Speeding".
+4. speedCheck(60, 90) should return "Ticket".
+5. speedCheck(65, 100) should return "Warning".
+6. speedCheck(88, 40) should return "Ticket".
+*/
+
+function speedCheck(speedMph, speedLimitKph) {
+  return speedMph;
+}
