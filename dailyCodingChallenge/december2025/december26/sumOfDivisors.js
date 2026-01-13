f5302fff0bfd81738ef87d526f5331863de05768
shxdi <234461025+shxdi@users.noreply.github.com>
2026-01-13 10:14:38 -0600

    Add provided information

--- dailyCodingChallenge/december2025/december26/sumOfDivisors.js
+++ dailyCodingChallenge/december2025/december26/sumOfDivisors.js
@@ -1,1 +1,22 @@
- 
+/*
+Given a positive integer, return the sum of all its divisors.
+
+A divisor is any integer that divides the number evenly (the remainder is 0).
+Only count each divisor once.
+
+For example, given 6, return 12 because the divisors of 6 are 1, 2, 3, and 6, and the sum of 
+    those is 12.
+*/
+
+/*
+1. sumDivisors(6) should return 12.
+2. sumDivisors(13) should return 14.
+3. sumDivisors(28) should return 56.
+4. sumDivisors(84) should return 224.
+5. sumDivisors(549) should return 806.
+6. sumDivisors(9348) should return 23520.
+*/
+
+function sumDivisors(n) {
+  return n;
+}
