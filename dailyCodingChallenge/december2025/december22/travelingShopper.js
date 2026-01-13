0b0af7bbe7268058b15360a6a338b1b6008a6683
shxdi <234461025+shxdi@users.noreply.github.com>
2026-01-13 09:41:40 -0600

    Finish travelingShopper

--- dailyCodingChallenge/december2025/december22/travelingShopper.js
+++ dailyCodingChallenge/december2025/december22/travelingShopper.js
@@ -31,3 +31,46 @@ Otherwise, return "Buy the first X items.", where X is the number of items you c
 5. buyItems(["200.00", "USD"], [["50.00", "USD"], ["40.00", "EUR"], ["30.00", "GBP"], 
     ["5000", "JPY"], ["25.00", "CAD"], ["20.00", "USD"]]) should return "Buy the first 5 items.".
 */
+
+function buyItems(funds, items) {
+  const eurMultiplier = 1.1;
+  const gbpMultiplier = 1.25;
+  const jpyMultiplier = 0.007;
+  const cadMultiplier = 0.75;
+  let currentFunds;
+  let purchasedItems = 0;
+
+  if (funds[1] === 'USD') {
+    currentFunds = funds[0];
+  } else if (funds[1] === 'EUR') {
+    currentFunds = funds[0] * eurMultiplier;
+  } else if (funds[1] === 'GBP') {
+    currentFunds = funds[0] * gbpMultiplier;
+  } else if (funds[1] === 'JPY') {
+    currentFunds = funds[0] * jpyMultiplier;
+  } else if (funds[1] === 'CAD') {
+    currentFunds = funds[0] * cadMultiplier;
+  }
+
+  for (let i = 0; i < items.length; i++) {
+    if (items[i][1] === 'USD') {
+      currentFunds -= items[i][0];
+    } else if (items[i][1] === 'EUR') {
+      currentFunds -= items[i][0] * eurMultiplier;
+    } else if (items[i][1] === 'GBP') {
+      currentFunds -= items[i][0] * gbpMultiplier;
+    } else if (items[i][1] === 'JPY') {
+      currentFunds -= items[i][0] * jpyMultiplier;
+    } else if (items[i][1] === 'CAD') {
+      currentFunds -= items[i][0] * cadMultiplier;
+    }
+
+    if (currentFunds > 0) {
+      purchasedItems++;
+    } else {
+      return `Buy the first ${purchasedItems} items.`;
+    }
+  }
+
+  return 'Buy them all!';
+}
